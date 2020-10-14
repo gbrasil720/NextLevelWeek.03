74cf0fde0f8f624635daf1c317a2267da3b9dd24
Guilherme <resendebrasilgui@gmail.com>
2020-10-14 16:57:02 -0300

    :tada: Aula #1, #2 e #3 completas!

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -53,7 +53,7 @@ export default {
     about,
     instructions,
     opening_hours,
-    open_on_weekends,
+    open_on_weekends: open_on_weekends === 'true',
     images
    }
 
@@ -76,7 +76,7 @@ export default {
      abortEarly: false
    })
    
-   const orphanage = orphanagesRepository.create(data)
+   const orphanage = orphanagesRepository.create(data) 
  
    await orphanagesRepository.save(orphanage)
  
